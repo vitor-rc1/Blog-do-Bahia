fd23322395170f3dd51fca2f75730b39f18f5dc9
Vitor Rodrigues <vitor_rc@outlook.com>
2020-10-20 20:40:03 -0300

    Criaçao do backend e alteraçao nas organizaçoes das pastas

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const { Client } = require('pg')
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send("Foi")
+})
+
+const client = new Client({
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'teste',
+  password: 'hdq001@@',
+  port: 5432,
+})
+client.connect()
+client.query('CREATE TABLE alunos (nome int)', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
+
+app.listen(port, () => {
+  console.log("Ta rodando o menino")
+})
+
+
