6287942e8c87ee424d7690569702f3f9677028c1
Vitor Rodrigues <vitor_rc@outlook.com>
2020-12-15 16:19:29 -0300

    Adicao das rotas do CRUD

--- server/server.js
+++ server/server.js
@@ -4,29 +4,83 @@ var cors = require('cors');
 const app = express();
 const port = 3001;
 
-const { Client } = require('pg')
+const { Pool } = require('pg')
 
 app.use(express.json())
 app.use(cors())
 
-app.post('/create', (req, res) => {
-  console.log(req.body)
-  res.send("Foi")
-})
 
-const client = new Client({
+const pool = new Pool({
   user: 'postgres',
   host: '127.0.0.1',
   database: 'teste',
   password: 'hdq001@@',
   port: 5432,
 })
-client.connect()
-client.query('CREATE TABLE posts (content int)', (err, res) => {
-  console.log(err, res)
-  client.end()
+
+// create table
+pool.query('CREATE TABLE IF NOT EXISTS posts (id SERIAL PRIMARY KEY, post jsonb)', (error) => {
+  console.log(error)
 })
 
+// create post
+app.post('/create', (req, res) => {
+  pool.query(`INSERT INTO posts(post) VALUES('${JSON.stringify(req.body)}')`, (error) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log('Data insert successful');
+  })
+  res.send("Data insert successful")
+})
+
+// acess all posts
+app.get('/posts', (req, res) => {
+  pool.query('SELECT * FROM posts', (error, result) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(result);
+    res.send(result.rows)
+  })
+})
+
+// acess especific post
+app.get('/:id/post', (req, res) => {
+  pool.query(`SELECT * FROM posts WHERE id=${req.params.id}`, (error, result) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(result);
+    res.send(result.rows)
+  })
+})
+
+// update specific post
+app.put('/:id/update', (req, res) => {
+  pool.query(`UPDATE posts SET post='${JSON.stringify(req.body)}' WHERE id=${req.params.id}`, (error, result) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    res.send("Update successful")
+  })
+})
+
+
+// delete especific post
+app.delete('/:id/delete', (req, res) => {
+  pool.query(`DELETE FROM posts WHERE id=${req.params.id}`, (error, result) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    res.send("Delete successful")
+  })
+})
 
 app.listen(port, () => {
   console.log("Ta rodando o menino")
